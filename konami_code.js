9ded8eb69263dc8fec93a2cfaca0009b7bb66c35
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-21 10:47:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 const main = document.body
 
-main.addEventListener('keydown', oneKeyDownHandler() {
-  alert('I was clicked!')
-})
+main.addEventListener('keydown', oneKeyDownHandler() )
+
 
 function init() {
   // Write your JavaScript code inside the init() function
