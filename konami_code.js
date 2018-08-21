c3690cc4e3fccaf975331e999fa5adafce04a62f
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-21 10:45:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 const main = document.body
- 
-main.addEventListener('click', function(event) {
+
+main.addEventListener('keydown', oneKeyDownHandler() {
   alert('I was clicked!')
 })
 
